6e80072bc8ca40020c20db5bd651bcc843968401
Johannes Feldmann <johannes.feldmann@pik-potsdam.de>
2012-11-06 15:12:42 -0900

    slight modification of subgrid grounding line interpolation now values in floating/ice free cells cannot exceed 1 anymore

--- src/base/iMgeometry.cc
+++ src/base/iMgeometry.cc
@@ -859,6 +859,8 @@ PetscErrorCode IceModel::sub_gl_position() {
   string subgltype = config.get_string("subgl_type");
 
   IceModelVec2S gl_mask_new = vWork2d[0];
+  IceModelVec2S gl_mask_unground_x = vWork2d[0];
+  IceModelVec2S gl_mask_unground_y = vWork2d[0];
   //ierr = gl_mask.copy_to(gl_mask_new); CHKERRQ(ierr);
   
   ierr =    vH.begin_access(); CHKERRQ(ierr);
@@ -866,8 +868,14 @@ PetscErrorCode IceModel::sub_gl_position() {
   ierr = vMask.begin_access(); CHKERRQ(ierr);
   ierr = gl_mask.begin_access(); CHKERRQ(ierr);
   ierr = gl_mask_new.begin_access(); CHKERRQ(ierr);
+  ierr = gl_mask_unground_x.begin_access(); CHKERRQ(ierr);
+  ierr = gl_mask_unground_y.begin_access(); CHKERRQ(ierr);
   
   ierr = gl_mask_new.set(0.0); CHKERRQ(ierr);
+  //gl_mask_unground_x/y state fraction of ungrounding in each, hence start with 1.0
+  //only used for floating or ice free regions
+  ierr = gl_mask_unground_x.set(1.0); CHKERRQ(ierr);
+  ierr = gl_mask_unground_y.set(1.0); CHKERRQ(ierr);
 
   for (PetscInt i = grid.xs; i < grid.xs + grid.xm; ++i) {
     for (PetscInt j = grid.ys; j < grid.ys + grid.ym; ++j) { 
@@ -894,8 +902,8 @@ PetscErrorCode IceModel::sub_gl_position() {
         if (interpol<0.5)
           gl_mask_x+=(interpol-0.5);
         else
-          gl_mask_new(i+1,j)+=(interpol-0.5);
-        
+	  gl_mask_unground_x(i+1,j)-=(interpol-0.5);
+	  
         ierr = verbPrintf(4, grid.com,"!!! PISM_INFO: type=%s, h1=%f, h2=%f, interpol=%f at i=%d, j=%d\n",subgltype.c_str(),xpart1,xpart2,interpol,i,j); CHKERRQ(ierr);
       }
       //if (mask.grounded(i, j) && mask.floating_ice(i-1, j)){
@@ -915,7 +923,7 @@ PetscErrorCode IceModel::sub_gl_position() {
           gl_mask_x+=(interpol-0.5);
         else{
           //if (vH(i-1, j)>0.0)
-            gl_mask_new(i-1,j)+=(interpol-0.5);
+            gl_mask_unground_x(i-1,j)-=(interpol-0.5);
         }  
         ierr = verbPrintf(4, grid.com,"!!! PISM_INFO: type=%s, h1=%f, h2=%f, interpol=%f at i=%d, j=%d\n",subgltype.c_str(),xpart1,xpart2,interpol,i,j); CHKERRQ(ierr);
       }     
@@ -935,7 +943,7 @@ PetscErrorCode IceModel::sub_gl_position() {
         if (interpol<0.5)
           gl_mask_y+=(interpol-0.5);
         else
-          gl_mask_new(i,j+1)+=(interpol-0.5);
+          gl_mask_unground_y(i,j+1)-=(interpol-0.5);
           
        ierr = verbPrintf(4, grid.com,"!!! PISM_INFO: type=%s, h1=%f, h2=%f, interpol=%f at i=%d, j=%d\n",subgltype.c_str(),xpart1,xpart2,interpol,i,j); CHKERRQ(ierr);
       }
@@ -955,7 +963,7 @@ PetscErrorCode IceModel::sub_gl_position() {
         if (interpol<0.5)
           gl_mask_y+=(interpol-0.5);
         else
-          gl_mask_new(i,j-1)+=(interpol-0.5);
+          gl_mask_unground_y(i,j-1)-=(interpol-0.5);
           
         ierr = verbPrintf(4, grid.com,"!!! PISM_INFO: type=%s, h1=%f, h2=%f, interpol=%f at i=%d, j=%d\n",subgltype.c_str(),xpart1,xpart2,interpol,i,j); CHKERRQ(ierr);
       }
@@ -964,11 +972,20 @@ PetscErrorCode IceModel::sub_gl_position() {
     } // inner for loop (j)
   } // outer for loop (i)
 
+  for (PetscInt i = grid.xs; i < grid.xs + grid.xm; ++i) {
+    for (PetscInt j = grid.ys; j < grid.ys + grid.ym; ++j) { 
+      if (mask.floating_ice(i,j) || mask.ice_free_ocean(i,j))
+	gl_mask_new(i,j) = 1.0 - gl_mask_unground_x(i,j) * gl_mask_unground_y(i,j);
+    }
+  }
+
   ierr =         vH.end_access(); CHKERRQ(ierr);
   ierr =       vbed.end_access(); CHKERRQ(ierr);
   ierr =      vMask.end_access(); CHKERRQ(ierr);
   ierr =     gl_mask.end_access(); CHKERRQ(ierr);
   ierr =     gl_mask_new.end_access(); CHKERRQ(ierr);
+  ierr =     gl_mask_unground_x.end_access(); CHKERRQ(ierr);
+  ierr =     gl_mask_unground_y.end_access(); CHKERRQ(ierr);
   
   // finally copy gl_mask_new into gl_mask and communicate ghosted values
   //ierr = gl_mask_new.beginGhostComm(gl_mask); CHKERRQ(ierr);
